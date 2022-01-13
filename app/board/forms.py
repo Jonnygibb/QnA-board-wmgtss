1f63138bcccdd6c63443580f11fe0ff370a802fb
u1921415 <jonathan.gibbons@warwick.ac.uk>
2022-01-13 13:52:29 +0000

    Created class based view for login page and tested sign up and log in

--- app/board/forms.py
+++ app/board/forms.py
@@ -2,8 +2,26 @@ from django import forms
 from .models import User
 
 class SignUpForm(forms.ModelForm):
+    """
+    Form representing sign in webpage
+    """
+    # Field for password input - typed characters will be hidden
     password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
+        # User model from models.py
         model = User
         fields = ['first_name', 'last_name', 'email', 'username', 'password']
+
+
+class LogInForm(forms.ModelForm):
+    """
+    Form representing login webpage
+    """
+    # Field for password input - typed characters will be hidden
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        # User model from models.py
+        model = User
+        fields = ['email', 'password']
